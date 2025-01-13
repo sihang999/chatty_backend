1f1abf6a53b4db06631d0299a1a0c27c861e48e5
Sihang Wu <128032705+sihang999@users.noreply.github.com>
2025-01-13 15:02:11 +0100

    Update run.py for cancel

--- run.py
+++ run.py
@@ -1,24 +1,7 @@
 from app import create_app
 from app.routes import register_routes
-from datetime import datetime
-from app.utils import manage_files
-from time import sleep
-import threading
-
-
-app = create_app()
-register_routes(app)
-def update_files_task():
-    while True:
-        now = datetime.now()
-        if now.hour == 2:  
-            manage_files()
-        sleep(2700)  
 
 if __name__ == '__main__':
-        
-    flask_thread = threading.Thread(target=update_files_task)
-    flask_thread.start()
-
+    app = create_app()
+    register_routes(app)
     app.run(host='0.0.0.0', port=5000, debug=True)
-
